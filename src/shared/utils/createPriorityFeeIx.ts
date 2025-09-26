7848cd85212975a4e5f22198e7be449473276e20
ibrica <ivbritvic@gmail.com>
2025-09-26 16:50:35 +0200

    feat: remove solana utils

--- src/shared/utils/createPriorityFeeIx.ts
+++
@@ -1,8 +0,0 @@
-import { ComputeBudgetProgram, TransactionInstruction } from '@solana/web3.js';
-import { priorityFee } from '../constants/solana';
-
-export const createPriorityFeeIx = (): TransactionInstruction => {
-  return ComputeBudgetProgram.setComputeUnitPrice({
-    microLamports: priorityFee, // Validator Tip (adjust for priority)
-  });
-};
--- src/shared/utils/solana/encoding.ts
+++
@@ -1,5 +0,0 @@
-import base58 from 'bs58';
-
-export const decodeTransactionData = (data: string): string => {
-  return base58.encode(Buffer.from(data, 'base64'));
-};
