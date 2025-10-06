716b271ec06a426da08521496d6acbf116f86827
ibrica <ivbritvic@gmail.com>
2025-10-06 15:09:42 +0200

    fix: remove leftover code

--- src/app/perps/Perp.module.ts
+++ src/app/perps/Perp.module.ts
@@ -3,13 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Perp, PerpSchema } from './Perp.schema';
 import { PerpService } from './Perp.service';
 import { PerpRepository } from './Perp.repository';
-import { PerpPriceMonitorService } from './PerpPriceMonitor.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Perp.name, schema: PerpSchema }]),
   ],
-  providers: [PerpService, PerpRepository, PerpPriceMonitorService],
-  exports: [PerpService, PerpPriceMonitorService],
+  providers: [PerpService, PerpRepository],
+  exports: [PerpService],
 })
 export class PerpModule {}
--- src/shared/utils/stringToKeypair.ts
+++
@@ -1,6 +0,0 @@
-import { Keypair } from '@solana/web3.js';
-import * as bs58 from 'bs58';
-
-export const stringToKeypair = (privateKey: string): Keypair => {
-  return Keypair.fromSecretKey(bs58.default.decode(privateKey));
-};
